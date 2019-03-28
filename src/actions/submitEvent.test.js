daf24e95c9c73737cf73c4d8285cc9114ea33827
Shaba9 <shaba92@gmail.com>
2019-03-28 11:10:23 -0700

    update event name passes

--- src/actions/submitEvent.test.js
+++ src/actions/submitEvent.test.js
@@ -3,7 +3,11 @@ import {
   CREATE_EVENT,
   CREATE_EVENT_PENDING,
   CREATE_EVENT_FULFILLED,
-  CREATE_EVENT_REJECTED
+  CREATE_EVENT_REJECTED,
+  updateContact,
+  UPDATE_CONTACT,
+  updateEventName,
+  UPDATE_EVENT_NAME
 } from './submitEvent';
 
 jest.mock('../services/eventsService');
@@ -22,6 +26,30 @@ describe('submit event actions', () => {
       fulfilledType: CREATE_EVENT_FULFILLED,
       rejectedType: CREATE_EVENT_REJECTED,
       payload: expect.any(Promise)
+    });
+  });
+
+  it('updates contact info', () => {
+    const action = updateContact({
+      name: 'The event',
+      email: 'blah@email.com'
+    });
+
+    expect(action).toEqual({
+      type: UPDATE_CONTACT,
+      payload: {
+        name: 'The event',
+        email: 'blah@email.com'
+      }
+    });
+  });
+
+  it('updates event name', () => {
+    const action = updateEventName('The New Name');
+
+    expect(action).toEqual({
+      type: UPDATE_EVENT_NAME,
+      payload: 'The New Name'
     });
   });
 });
