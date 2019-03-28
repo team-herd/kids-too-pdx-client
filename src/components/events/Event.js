f564cf545f6b6f735eae25dbcdb902c2f953bea0
Shaba9 <shaba92@gmail.com>
2019-03-28 16:25:35 -0700

    AdminEvent snapshot test passes

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 function Event({ event }) {
   const date = new Date(event.date);
-  event.dateDisplay = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  const dateDisplay = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 
   return (
 
@@ -13,7 +13,7 @@ function Event({ event }) {
       <section className={styles.Event}>
         <img src={event.image} alt='event picture' />
         <Link to={`/events/${event._id}`}><h2>{event.name}</h2></Link>
-        <p><span>Date:</span>{event.dateDisplay}</p>
+        <p><span>Date:</span>{dateDisplay}</p>
         <p><span>Time: @</span> {event.time}</p>
         {event.location && <p><span>Location:</span>{event.location.venue}</p>}
         <p><span>Cost:</span>{event.price}</p>
