5573c220a505e6d9f19d90c1e4abd26ac967b4cc
Dyanna Tolman <dyannast@gmail.com>
2019-03-28 16:05:41 -0700

    added standard background image changed link color and started user stories

---
+++ UserStories.md
@@ -0,0 +1,7 @@
+KIDS 2 USER STORIES:
+
+User 1:
+Barbara Johnson, 65 Retired
+Watches grandkids twice a week and on Holidays.
+Not tech savvy and wants simplicity and ease when doing a search online
+Interested in 
--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Event.css';
+// import EventDetail from '../events/EventDetail';
 import { Link } from 'react-router-dom';
 
 // import dollar from '../../../public/assets/dollar-symbol.png';
-function Event({ event }) {
+function Event({ event, details }) {
   // const { name, date, time, price, ageMin, ageMax, description, category, image } = event;
   // if(event.cost === 'free') {
   //   return <img src={dollar} alt="dollar sign"/>;
@@ -18,17 +19,18 @@ function Event({ event }) {
         <img src={event.image} alt='event picture' />
         <Link to={`/events/${event._id}`}><h2>{event.name}</h2></Link>
         <p><span>Date:</span>{event.date}</p>
-        <p><span>Time: @</span> {event.time}</p>
+        <p><span>Time:</span>{event.time}</p>
         {event.location && <p><span>Location:</span>{event.location.venue}</p>}
         <p><span>Cost:</span>{event.price}</p>
         <p><span>Age:</span>{event.ageMin}-{event.ageMax}</p>
         <p>{event.category}</p>
-        <p>{event.description}</p>
+        {details && event.description && <p>{event.description}</p>}
       </section>
     </>
   );
 }
 Event.propTypes = {
-  event: PropTypes.object.isRequired
+  event: PropTypes.object.isRequired,
+  details: PropTypes.boolean
 };
 export default Event;
