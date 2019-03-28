c7351195d8088584139153700e500975b57beb3d
Shaba9 <shaba92@gmail.com>
2019-03-28 10:51:57 -0700

    fetch filtered events passes

--- src/actions/events.test.js
+++ src/actions/events.test.js
@@ -3,7 +3,12 @@ import {
   FETCH_EVENTS,
   FETCH_EVENTS_PENDING,
   FETCH_EVENTS_FULFILLED,
-  FETCH_EVENTS_REJECTED
+  FETCH_EVENTS_REJECTED,
+  fetchFilteredEvents,
+  FETCH_FILTERED_EVENTS_PENDING,
+  FETCH_FILTERED_EVENTS_FULFILLED,
+  FETCH_FILTERED_EVENTS_REJECTED,
+  FETCH_FILTERED_EVENTS,
 } from './events';
 
 jest.mock('../services/eventsService');
@@ -16,6 +21,20 @@ describe('actions test', () => {
       pendingType: FETCH_EVENTS_PENDING,
       fulfilledType: FETCH_EVENTS_FULFILLED,
       rejectedType: FETCH_EVENTS_REJECTED,
+      payload: expect.any(Promise)
+    });
+  });
+
+  it('fetches filtered events', () => {
+    const action = fetchFilteredEvents({
+      ageMin: 12,
+      ageMax: 15
+    });
+    expect(action).toEqual({
+      type: FETCH_FILTERED_EVENTS,
+      pendingType: FETCH_FILTERED_EVENTS_PENDING,
+      fulfilledType: FETCH_FILTERED_EVENTS_FULFILLED,
+      rejectedType: FETCH_FILTERED_EVENTS_REJECTED,
       payload: expect.any(Promise)
     });
   });
