211285f7c0bd4ef9bf04c5199f9cfd9c616aaa40
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-28 08:38:53 -0700

    css for navbar, and dt view

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,30 +14,30 @@ import img from '../../../';
 function Header() {
   return (
     <>
-    <section className={styles.logo}>
-      <img src='../../../public/assets/logo.png' /> 
-    </section>
-           <Router>
-             <section>
-               <header className={styles.Header}>
-                 <nav>
-                   <Link to='/'>Home</Link>
-                   <Link to='/submit-event'>Submit Event</Link>
-                   <Link to='/contact'>Contact</Link>
-                 </nav>
-               </header>
-               <Switch>
-                 <Route exact path='/' component={Home} />
-                 <Route exact path='/submit-event' component={withSession(SubmitEventContainer)} />
-                 <Route exact path='/events/:id' component={EventDetail} />
-                 <Route exact path='/admin' component={AdminEvents} />
-                 <Route exact path='/admin/event/:id' component={AdminEvents} />
-                 <Route exact path='/contact' component={Contact} /> 
-                 <Route exact path='/aboutus' component={AboutUs} /> 
-                 <Route exact path='/callback' component={Callback} /> 
-               </Switch>
-             </section>
-           </Router>
+      <Router>
+        <section className={styles.container}>
+          <header className={styles.Header}>
+            <section className={styles.logo}>
+              <img src='../../../public/assets/logo.png' />
+            </section>
+            <nav className={styles.nav}>
+              <Link to='/'>Home</Link>
+              <Link to='/submit-event'>Submit Event</Link>
+              <Link to='/contact'>Contact</Link>
+            </nav>
+          </header>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/submit-event' component={withSession(SubmitEventContainer)} />
+            <Route exact path='/events/:id' component={EventDetail} />
+            <Route exact path='/admin' component={AdminEvents} />
+            <Route exact path='/admin/event/:id' component={AdminEvents} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/aboutus' component={AboutUs} />
+            <Route exact path='/callback' component={Callback} />
+          </Switch>
+        </section>
+      </Router>
     </>
   );
 }
