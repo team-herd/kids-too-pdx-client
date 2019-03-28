ab1d0daf58ee29bd0da2a216ae186af3e8128cad
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-28 15:53:25 -0700

    event age and css

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Event.css';
 import { Link } from 'react-router-dom';
-
+// import EventDescription from './EventDescription';
 // import dollar from '../../../public/assets/dollar-symbol.png';
 function Event({ event }) {
   // const { name, date, time, price, ageMin, ageMax, description, category, image } = event;
@@ -10,20 +10,24 @@ function Event({ event }) {
   //   return <img src={dollar} alt="dollar sign"/>;
   // }
   // console.log(event.location, 'location');
-
+  if(event.ageMin && event.ageMax) {
+    event.age = `${event.ageMin}-${event.ageMax}`;
+  } else if(event.ageMin) {
+    event.age = `${event.ageMin}`;
+  } else if(event.ageMax) {
+    event.age = `${event.ageMax}`;
+  }
   return (
-
     <>
       <section className={styles.Event}>
         <img src={event.image} alt='event picture' />
         <Link to={`/events/${event._id}`}><h2>{event.name}</h2></Link>
         <p><span>Date:</span>{event.date}</p>
-        <p><span>Time: @</span> {event.time}</p>
-        {event.location && <p><span>Location:</span>{event.location.venue}</p>}
-        <p><span>Cost:</span>{event.price}</p>
-        <p><span>Age:</span>{event.ageMin}-{event.ageMax}</p>
-        <p>{event.category}</p>
-        <p>{event.description}</p>
+        {event.time && <p><span>Time:</span> {event.time}</p>}
+        {event.location && event.location.venue && <p><span>Location:</span>{event.location.venue}</p>}
+        {event.price && <p><span>Cost:</span>{event.price}</p>}
+        {event.age && <p><span>Age:</span> {event.age}</p>}
+        {event.category && <p>{event.category}</p>}
       </section>
     </>
   );
